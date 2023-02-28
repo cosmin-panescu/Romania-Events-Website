092c9dd6c494c9fb0720233a27857ab88cb10c15
Panescu Cosmin <1panescu.cosmin@gmail.com>
2023-02-28 10:33:56 +0200

    Adding cities (and their descriptions) as main categories for classifying festivals.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com', 'hydeparkwinterwonderland.com', 'wembleypark.com', 'cdn.pixabay.com'],
+    domains: ['images.unsplash.com', 'hydeparkwinterwonderland.com', 'wembleypark.com', 'cdn.pixabay.com', 'img5.goodfon.com', 'media.istockphoto.com', 'e1.pxfuel.com', 'w0.peakpx.com', 'free4kwallpapers.com', 'discoverdobrogea.ro', 'p1.pxfuel.com', 'www.destinatii.info', 'www.forbes.ro', 'www.crafted-tours-romania.com', 'romtur.ro', 'www.romaniaforall.it', 'romaniatourism.com', 'i.guim.co.uk', 'xn--urlaub-in-rumnien-2qb.de', 'content.r9cdn.net', 'mediaim.expedia.com' ,''],
   },
 };
 
