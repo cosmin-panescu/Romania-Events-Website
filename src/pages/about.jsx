8461ca376ad2a1c09b975999e3b786c85cf443e0
Panescu Cosmin <1panescu.cosmin@gmail.com>
2023-03-05 12:51:33 +0200

    Add content to About page and style it

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,8 +3,21 @@ import React from 'react'
 const About = () => {
   return (
     <div className='about'>
-      <h1>About Us</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora, ab nulla! Perferendis sequi reprehenderit est voluptatem repellendus perspiciatis vitae placeat? Deleniti laboriosam laudantium blanditiis delectus? Alias nisi quibusdam delectus quos eligendi temporibus, ipsa dolores illum dolorum ea beatae facere accusamus.</p>
+      <div className="about-content">
+        <h1>About Us</h1>
+        <p>
+         📌 Welcome to my website, dedicated to showcasing the vibrant and diverse festival scene in Romania. I am passionate about promoting Romanian culture and tourism, and believe that the country's festivals are one of its greatest assets.
+        </p>
+        <p>
+          📌 This website provides a comprehensive list of all the major festivals taking place in Romania throughout the year, from traditional festivals to modern music events. Whether you're a local or a tourist, I hope that this website will inspire you to discover new experiences and participate in the rich cultural traditions of Romania.
+        </p>
+        <p>
+          📌 I share a love for Romanian culture and festivals. We believe that festivals are not just a time to celebrate, but also an opportunity to learn about different customs, beliefs, and ways of life. I created this website as a way to share this passion with others and to help promote the country's culture.
+        </p>
+        <p>
+          📌 I believe that festivals play an important role in promoting cultural exchange and understanding, and hope that this website will encourage more people to visit Romania and experience its rich cultural traditions. We invite you to explore my website and discover the many festivals that Romania has to offer. Thank you for visiting!
+        </p>
+      </div>
     </div>
   )
 }
