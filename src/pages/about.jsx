d7ec2b05bda270c3583c857d5be7cfafa96be709
Panescu Cosmin <1panescu.cosmin@gmail.com>
2023-03-17 11:42:34 +0200

    remove unwanted symbols

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,16 +6,16 @@ const About = () => {
       <div className="about-content">
         <h1>About Us</h1>
         <p>
-          Welcome to my website, dedicated to showcasing the vibrant and diverse festival scene in Romania. I am passionate about promoting Romanian culture and tourism, and believe that the country's festivals are one of its greatest assets.
+         📌 Welcome to my website, dedicated to showcasing the vibrant and diverse festival scene in Romania. I am passionate about promoting Romanian culture and tourism, and believe that the festivals are one of its greatest assets.
         </p>
         <p>
-          This website provides a comprehensive list of all the major festivals taking place in Romania throughout the year, from traditional festivals to modern music events. Whether you're a local or a tourist, I hope that this website will inspire you to discover new experiences and participate in the rich cultural traditions of Romania.
+          📌 This website provides a comprehensive list of all the major festivals taking place in Romania throughout the year, from traditional festivals to modern music events. Whether you are a local or a tourist, I hope that this website will inspire you to discover new experiences and participate in the rich cultural traditions of Romania.
         </p>
         <p>
-          I share a love for Romanian culture and festivals. We believe that festivals are not just a time to celebrate, but also an opportunity to learn about different customs, beliefs, and ways of life. I created this website as a way to share this passion with others and to help promote the country's culture.
+          📌 I share a love for Romanian culture and festivals. We believe that festivals are not just a time to celebrate, but also an opportunity to learn about different customs, beliefs, and ways of life. I created this website as a way to share this passion with others and to help promote the culture of Romania.
         </p>
         <p>
-          I believe that festivals play an important role in promoting cultural exchange and understanding, and hope that this website will encourage more people to visit Romania and experience its rich cultural traditions. We invite you to explore my website and discover the many festivals that Romania has to offer. Thank you for visiting!
+          📌 I believe that festivals play an important role in promoting cultural exchange and understanding, and hope that this website will encourage more people to visit Romania and experience its rich cultural traditions. We invite you to explore my website and discover the many festivals that Romania has to offer. Thank you for visiting!
         </p>
       </div>
     </div>
