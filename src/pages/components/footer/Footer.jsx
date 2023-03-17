9a3f783256d6c6dbc235dd02d8bcac79128a585b
Panescu Cosmin <1panescu.cosmin@gmail.com>
2023-03-17 11:04:40 +0200

    change footer text and make it responsive

--- src/pages/components/footer/Footer.jsx
+++ src/pages/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer>
-            <p>© Panescu Cosmin - A project to promote Romania</p>
+            <p>© 2023 Panescu Cosmin - A project to promote Romania</p>
         </footer>)
 }
 
